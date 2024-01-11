e3531cada0d4691ac51bb34853b59ec7357bc9d2
Maxence Poizat <maxence.poizat@etudiant.univ-reims.fr>
2024-01-11 16:10:35 +0100

    Ajout de TestPlateau et TestCouleur

---
+++ tests/TestCouleur.py
@@ -0,0 +1,15 @@
+from Model.Constantes import *
+from Model.Plateau import *
+from Model.Pion import *
+
+p = construirePlateau()
+print(p)
+pion = construirePion(const.JAUNE)
+line = placerPionPlateau(p, pion, 2)
+print("Placement d’un pion en colonne 2. Numéro de ligne :", line)
+print(p)
+
+# Essais sur les couleurs
+print("\x1B[43m \x1B[0m : carré jaune ")
+print("\x1B[41m \x1B[0m : carré rouge ")
+print("\x1B[41mA\x1B[0m : A sur fond rouge")
